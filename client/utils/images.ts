d812d904b4c7cad4d31819d34871b9b72090be66
rasna-skumar1306 <rasna5202@gmail.com>
2022-03-06 00:14:55 +0530

    utils for images

---
+++ client/utils/images.ts
@@ -0,0 +1,11 @@
+import EmptySeat from "../assets/emptySeat.svg";
+import SelectedSeat from "../assets/selectedSeat.svg";
+import BookedSeat from "../assets/bookedSeat.svg";
+
+const images = {
+  EmptySeat,
+  SelectedSeat,
+  BookedSeat,
+};
+
+export default images;
